1f98080bb4c9faf5ca2000c8a58ad696084d1dd6
sefe <sebastian.ferngren@gmail.com>
2019-08-01 10:07:10 +0200

    refactor, optimizes raycaster by only checking each new tile instead of fixed intervals

--- RayCaster.h
+++ RayCaster.h
@@ -23,8 +23,76 @@ struct RayResult
 class RayCaster
 {
 public:
+	RayResult RayDistanceEx(IMapReader* reader, float px, float py, float dx, float dy)
+	{ 
+		int mapx = px;
+		int mapy = py;
+ 
+		int stepx = dx > 0 ? 1 : -1;
+		int stepy = dy > 0 ? 1 : -1;
+ 
+		float rdx = abs(1.0f / dx);
+		float dry = abs(1.0f / dy);
+
+		float sideDistX = 0.0f;
+		float sideDistY = 0.0f;
+
+		if (dx < 0)
+		{ 
+			sideDistX = (px - mapx) * rdx;
+		}
+		else
+		{ 
+			sideDistX = (mapx + 1.0f - px) * rdx;
+		}
+
+		if (dy < 0)
+		{
+			sideDistY = (py - mapy) * dry;
+		}
+		else 
+		{
+			sideDistY = (mapy + 1.0f - py) * dry; 
+		}
+ 
+
+		bool hit = false;
+		bool side = false;
+
+		while (!hit)
+		{
+			if (sideDistX < sideDistY)
+			{ 
+				sideDistX += rdx;
+				mapx += stepx;
+				side = false;
+			}
+			else
+			{ 
+				sideDistY += dry;
+				mapy += stepy;
+				side = true;
+			}
+
+			int tile = reader->ReadTileAtPos(mapx, mapy);
+			hit = reader->IsSolid(tile);
+		}
+
+		float dist = side ? ((float)mapy - py + (1.0f - stepy) / 2.0f) / dy :
+			((float)mapx - px + (1.0f - stepx) / 2.0f) / dx;
+
+		float distx = ((float)mapx - px + (1.0f - stepx) / 2.0f) / dx;
+		float disty =  ((float)mapy - py + (1.0f - stepy) / 2.0f) / dy;
+
+		float hitx = px + (dx * dist);
+		float hity = py + (dy * dist);
+		return RayResult{ dist,0.0,hitx,hity };
+	}
+
+
 	RayResult RayDistance(IMapReader* reader, float px, float py, float dx, float dy)
 	{
+		return RayDistanceEx(reader, px, py, dx, dy);
 		glm::vec2 pos;
 		pos.x = px;
 		pos.y = py;
