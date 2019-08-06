c6214b95f3d82b17c98e7179a160361952924cf0
sefe <sebastian.ferngren@gmail.com>
2019-08-06 21:50:19 +0200

    refactor, moves animation to its own file. not wolf.h

--- wolf.h
+++ wolf.h
@@ -1,38 +1,3 @@
 #pragma once
 
 #include "resource.h"
-#include <vector>
-
-struct Frame
-{
-	int x;
-	int y;
-	int w;
-	int h;
-};
-
-
-class Animation
-{
-public:
-	float Speed;
-	std::vector<Frame> AnimationStrip;
-
-	static Animation CreateWithFrames(int startx, int starty, int sw, int sh, int offsetx, int offsety, int dx, int dy, int length)
-	{
-		Animation anim;
-		anim.AnimationStrip.clear();
-		for (int i = 0; i < length; i++)
-		{
-			anim.AnimationStrip.push_back(anim.CreateFrameForCoord(startx + i * dx,starty + i * dy,sw,sh,offsetx,offsety));
-		}
-
-		return anim;
-	}
-
-private:
-	Frame CreateFrameForCoord(int sx, int sy, int sw, int sh, int xoffset, int yoffset)
-	{
-		return { sx * sw + (sx * xoffset),sy * sw + (yoffset * sy),sw,sh };
-	}
-};
